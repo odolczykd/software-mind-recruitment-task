1d36e6632474a7286d6b3ecf453c9fee494c1a6d
Dawid Odolczyk <dawid.odolczyk@gmail.com>
2025-01-28 15:52:56 +0100

    init commit

---
+++ backend/src/test/java/com/softwaremind/odolczykd/recruitment/SoftwareMindRecruitmentTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.softwaremind.odolczykd.recruitment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoftwareMindRecruitmentTaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
